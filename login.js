a5a6ff1525023cbac6c9d13f70b3806551791e4e
Érick Augusto Paludo <100646545+ErickPaludo@users.noreply.github.com>
2023-10-29 22:56:21 -0300

    #

--- login.js
+++ login.js
@@ -3,8 +3,8 @@ function Login(){
     var user = document.querySelector('#user').value;
     var pass = document.querySelector('#password').value;  
     alert(`${user} / ${pass}`);
-    if (user === "admin" && pass === "123") {
-        alert("Senha correta");
+    if (user.toLowerCase() === "admin" && pass === "123") {
+        window.open("Home/inicio.html", "_self");
     }
     else{
         alert("Senha Incorreta");
