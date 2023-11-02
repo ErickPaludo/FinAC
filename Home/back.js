83be1e478a381214f45571b1a189485de337e715
Érick Augusto Paludo <100646545+ErickPaludo@users.noreply.github.com>
2023-11-02 12:01:45 -0300

    Add files via upload

---
+++ Home/back.js
@@ -0,0 +1,17 @@
+function background() {
+    var overlay = document.createElement("div");
+    overlay.style.position = "fixed";
+    overlay.style.top = "0";
+    overlay.style.left = "0";
+    overlay.style.width = "100%";
+    overlay.style.height = "100%";
+    overlay.style.backgroundColor = "rgba(0, 0, 0, 0.8"; 
+    document.body.appendChild(overlay);
+  }
+  
+  function Reg_Saldo(){
+  background();
+  }
+  function Reg_Div(){
+    background();
+  }
